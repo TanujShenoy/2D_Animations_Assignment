f1999ab398ea89f2f07ec26317c33a7db9d6f577
TanujShenoy <98006225+TanujShenoy@users.noreply.github.com>
2022-02-04 21:53:22 +0530

    Commit_1

---
+++ Mario/Assets/Script/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+    [Header("Horizontal Movement")]
+    public float moveSpeed = 10f;
+    public Vector2 direction;
+
+    [Header("Components")]
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+    void FixedUpdate()
+    {
+        moveCharacter(direction.x);
+    }
+    void moveCharacter(float horizontal)
+    {
+        rb.AddForce(Vector2.right * horizontal * moveSpeed);
+        animator.SetFloat("Horizontal", Mathf.Abs(rb.velocity.x));
+    }
+}
